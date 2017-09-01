6703cb2d76e5b40de6d0c4f131bfc61ce766fd6f
MrRoobot <andihilzenthaler@gmail.com>
2017-09-01 15:35:49 +0200

    Prediction seems to work

--- src/NaiveBayes.java
+++ src/NaiveBayes.java
@@ -1,7 +1,9 @@
+import weka.classifiers.Evaluation;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ArffLoader;
 import weka.classifiers.bayes.NaiveBayesUpdateable;
+import weka.core.converters.ConverterUtils;
 
 import java.io.File;
 
@@ -23,9 +25,15 @@ public class NaiveBayes {
     public static void main(String[] args) throws Exception {
         // load data
         ArffLoader loader = new ArffLoader();
-        loader.setFile(new File("C:\\Users\\Andih\\IdeaProjects\\Informationssysteme\\TestDaten.arff"));
+        loader.setFile(new File("C:\\Users\\Andih\\IdeaProjects\\Informationssysteme\\TrainingsDaten.arff"));
         Instances structure = loader.getStructure();
         structure.setClassIndex(structure.numAttributes()-1);
+        ConverterUtils.DataSource source2 = new ConverterUtils.DataSource("C:\\Users\\Andih\\IdeaProjects\\Informationssysteme\\TestDaten.arff");
+        Instances test = source2.getDataSet();
+
+        if (test.classIndex() == -1)
+            test.setClassIndex(structure.numAttributes() - 1);
+
 
         // train NaiveBayes
         NaiveBayesUpdateable nb = new NaiveBayesUpdateable();
@@ -33,8 +41,10 @@ public class NaiveBayes {
         Instance current;
         while ((current = loader.getNextInstance(structure)) != null)
             nb.updateClassifier(current);
+        double label = nb.classifyInstance(test.instance(0));
+        test.instance(0).setClassValue(label);
 
         // output generated model
-        System.out.println(nb);
+        System.out.println(test.instance(0).stringValue(4));
     }
 }
